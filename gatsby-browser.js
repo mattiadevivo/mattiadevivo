523edf12a18acaefb13723ad76175848942a7305
mattiadevivo <mattiadevivo@gmail.com>
2021-02-17 22:09:23 +0100

    Added SEO, G-Analytics and fontawesome icons

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 import "./src/styles/global.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // This file is used by Gatsby to load files you can insert CDN and other stuff like this
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,64 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+// Options for contentful CMS
+options = {
+  accessToken: process.env.GATSBY_ACCESS_TOKEN,
+  spaceId: process.env.GATSBY_SPACE_ID,
+}
+if(process.env.NODE_ENV === "development"){
+  options.host = "preview.contentful.com";
+}
+
 module.exports = {
   siteMetadata: {
-    title: "mattiadevivo",
+    title: "Mattia De Vivo - Developer",
+    siteUrl: "https://mattiadevivo.netlify.app", 
+    description: "Mattia De Vivo, software developer and computer science engineer based in Treviso (Italy). I am actually focusing on cybersecurity.",
+    keywords: ["mattia de vivo", "mattiadevivo", "developer", "software developer", "sviluppatore software", "cybersecurity", "treviso"],
+    author: "Mattia De Vivo",
+    image: "/images/icon.png",
   },
   plugins: [
     {
       resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: `${process.env.GATSBY_ACCESS_TOKEN}`,
-        spaceId: `${process.env.GATSBY_SPACE_ID}`,
-      },
+      options: { accessToken: process.env.GATSBY_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_SPACE_ID,},
     },
     "gatsby-plugin-sharp",
     "gatsby-plugin-offline",
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [
+          process.env.GATSBY_GTAG, // Google Analytics / GA
+        ],
+        gtagConfig: {
+          anonymize_ip: false,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-manifest",  // PWA manifest
+      options: {
+        icon: "src/images/icon.png",
+        start_url: '/',
+        name: "mattiadevivo",
+        short_name: "MDV",
+        description: "Application showing Mattia De Vivo website",
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",

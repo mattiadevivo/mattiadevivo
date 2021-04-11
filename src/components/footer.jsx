4ec4655143798beb583751cb3031c490557869bf
Mattia De Vivo <mattiadevivo@gmail.com>
2021-04-11 09:49:51 +0200

    Changed footer style. Changed Experience entries order.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
     return(
         <footer>
-            
+            <div className="text-center p-3">
+                © mattiadevivo
+            </div>
         </footer>
     );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,7 @@ import { CookieBanner } from '@palmabit/react-cookie-law';
 import ExperienceEntry from "../components/experience-entry";
 import AboutSection from "../components/about";
 import ContactSection from "../components/contact";
+import Footer from "../components/footer";
 import Seo from "../components/seo";
 
 const IndexPage = ({ data }) => {
@@ -25,59 +26,60 @@ const IndexPage = ({ data }) => {
       </Seo>
       <Navbar />
       <main className="pt-3">
-      <AboutSection
-        image={person.image}
-        name={person.name}
-        about={person.about}
-      />
-      <Jumbotron
-        style={{ marginLeft: "10%", marginRight: "10%", marginBottom: "8px" }}
-      >
-        <Container>
-          <h2>Experience</h2>
-          <ul>
-            {experiences.map((item) => (
-              <ExperienceEntry
-                key={item.node.id}
-                position={item.node.position}
-                company={item.node.company}
-                startDate={item.node.startDate}
-                endDate={item.node.endDate}
-                location={item.node.location}
-                description={item.node.description}
-                works={item.node.works}
-              />
-            ))}
-          </ul>
-        </Container>
-      </Jumbotron>
-      <Jumbotron style={{ marginLeft: "10%", marginRight: "10%" }}>
-        <Container>
-          <h2>Education</h2>
-          <ul>
-            {educations.map((item) => (
-              <EducationEntry
-                key={item.node.id}
-                title={item.node.title}
-                institute={item.node.institute}
-                startDate={item.node.startDate}
-                endDate={item.node.endDate}
-                location={item.node.location}
-                mark={item.node.mark}
-                description={item.node.description}
-              />
-            ))}
-          </ul>
-        </Container>
-      </Jumbotron>
-      <ContactSection
-        address={person.address}
-        phone={person.phone}
-        email={person.email}
-      />
+        <AboutSection
+          image={person.image}
+          name={person.name}
+          about={person.about}
+        />
+        <Jumbotron
+          style={{ marginLeft: "10%", marginRight: "10%", marginBottom: "8px" }}
+        >
+          <Container>
+            <h2>Experience</h2>
+            <ul>
+              {experiences.map((item) => (
+                <ExperienceEntry
+                  key={item.node.id}
+                  position={item.node.position}
+                  company={item.node.company}
+                  startDate={item.node.startDate}
+                  endDate={item.node.endDate}
+                  location={item.node.location}
+                  description={item.node.description}
+                  works={item.node.works}
+                />
+              ))}
+            </ul>
+          </Container>
+        </Jumbotron>
+        <Jumbotron style={{ marginLeft: "10%", marginRight: "10%" }}>
+          <Container>
+            <h2>Education</h2>
+            <ul>
+              {educations.map((item) => (
+                <EducationEntry
+                  key={item.node.id}
+                  title={item.node.title}
+                  institute={item.node.institute}
+                  startDate={item.node.startDate}
+                  endDate={item.node.endDate}
+                  location={item.node.location}
+                  mark={item.node.mark}
+                  description={item.node.description}
+                />
+              ))}
+            </ul>
+          </Container>
+        </Jumbotron>
+        <ContactSection
+          address={person.address}
+          phone={person.phone}
+          email={person.email}
+        />
+        <Footer />
       </main>
       <CookieBanner
-        message="Questo sito utilizza cookies tecnici e analytics anonimi di terze parti per offrirti la migliore qualità d'utilizzo."
+        message="Questo sito utilizza dei cookie per monitorare e personalizzare l'esperienza di navigazione degli utenti. Continuando a navigare si autorizza l'utilizzo dei cookie su questo sito."
         wholeDomain={true}
         policyLink="https://www.cookiesandyou.com/"
         privacyPolicyLinkText="Per saperne di più"
@@ -86,11 +88,38 @@ const IndexPage = ({ data }) => {
         showMarketingOption={false}
         managePreferencesButtonText="Preferenze"
         savePreferencesButtonText="Salva e chiudi"
-        acceptButtonText="Capisco"
+        acceptButtonText="Accetto"
         onAccept={() => { }}
         onAcceptPreferences={() => { }}
         onAcceptStatistics={() => { }}
         onAcceptMarketing={() => { }}
+        styles={{
+          dialog: {
+            position: "fixed",
+            width: "100%",
+            bottom: "0%",
+            left: "0%",
+            right: "0%",
+            zIndex: "100000",
+            backgroundColor: "rgba(0,0,0, 0.8)",
+            padding: "10px"
+          },
+          message: {
+            color: "white",
+            fontSize: "small"
+          },
+          button: {
+            color: "#FFFFFF",
+            fontSize: "small",
+            backgroundColor: "#00B0ED",
+            border: "1px solid #00B0ED",
+            margin: "2px"
+          },
+          policy: {
+            color: "#FFFFFF",
+            fontSize: "small"
+          }
+        }}
       />
     </React.Fragment>
   );
@@ -117,7 +146,7 @@ export const eduQuery = graphql`
         }
       }
     }
-    allContentfulExperience(sort: { fields: startDate, order: DESC }) {
+    allContentfulExperience(sort: { fields: [endDate, startDate], order: DESC }) {
       edges {
         node {
           company
